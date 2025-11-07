d8ab9c6c30e42ee941f6a2bb7d20abe6943bd94e
Nataniel Jayaseelan <63678990+natanielj@users.noreply.github.com>
2025-11-07 02:06:32 +0000

    summer changes update

---
+++ end-to-end-driving/parse_tf.py
@@ -0,0 +1,3 @@
+import tensorflow as tf
+
+training_tf = ["~/work/data/waymo-e2e/waymo_open_dataset_end_to_end_camera_v_1_0_0/training_202504031202_202504151040.tfrecord-00000-of-00263"]
---
+++ setup_scripts/setup_waymo.sh
@@ -0,0 +1,2 @@
+gcloud auth login
+gcloud storage cp -r gs://waymo_open_dataset_v_1_4_1 ./data/waymo_e2e
